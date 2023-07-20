fc726d8ff9374cd07d066aa2be3693e47f32fef3
samanazadi1996@gmail.com <samanazadi1996@gmail.com>
2023-07-20 18:03:14 +0330

    .

---
+++ Src/Web/Jadval.Web/Areas/AdminPanel/Controllers/CrosswordController.cs
@@ -0,0 +1,25 @@
+﻿using Jadval.Application.Features.Crosswords.Queries.GetPagedListCrossword;
+using Jadval.Application.Wrappers;
+using Jadval.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace TSPStore.WebMvc.Areas.AdminPanel.Controllers
+{
+    public class CrosswordController : AdminBaseController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public async Task<IActionResult> _Index(GetPagedListCrosswordQuery model)
+        {
+            var serviceResult = await Mediator.Send(model);
+            var pageResult = new PagentaionResult<GetPagedListCrosswordQuery, PagedResponse<bool>>(model, serviceResult);
+
+            return PartialView(pageResult);
+        }
+
+    }
+
+}
